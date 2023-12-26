77feeddad5e654a9804ecf0bf124412f4ac0c230
tetuomi <kondo.tetta851@mail.kyutech.jp>
2023-12-26 14:53:27 +0900

    Can set margin of preprocessing

--- data_loader.py
+++ data_loader.py
@@ -26,8 +26,14 @@ class LoadDataset(data.Dataset):
         self.data_list = data_list
         self.da_rate = da_rate
         self.da_transform = Compose([
+                            # 垂直水平シフト
                             transforms.RandomAffine(degrees=0., translate=(0.2, 0.2), fill=1., interpolation=InterpolationMode.BILINEAR),
-                            ])
+
+                            # 垂直水平シフト＋拡大縮小
+                            # transforms.Lambda(lambda t: 1 - (t + 1) * 0.5), # -1 -> 1, 1 -> 0
+                            # transforms.RandomAffine(degrees=0., scale=(0.8, 1.2), translate=(0., 0.2), fill=0., interpolation=InterpolationMode.BILINEAR),
+                            # transforms.Lambda(lambda t: 2 * (1 - t) - 1), # 1 -> -1, 0 -> 1
+                        ])
 
     def __len__(self):
         return len(self.data_list)
@@ -39,13 +45,13 @@ class LoadDataset(data.Dataset):
         feature = self.data_list[index][1]
         return img, feature
 
-def read_img(path, image_size):
+def read_img(path, image_size, margin=5):
     img = cv2.imread(path, 0)
-    img = preprocessing(img, img_size=image_size, margin=5)
+    img = preprocessing(img, img_size=image_size, margin=margin)
 
     return transform(img).float()
 
-def make_data_list_google_font(image_size, num_style, encoder_path, encoder_zdim):
+def make_data_list_google_font(image_size, num_style, encoder_path, encoder_zdim, margin=5):
     data_list = {'train': [], 'test': []}
     df = pd.read_csv('csv_files/google_fonts_drop_none.csv')
 
@@ -62,7 +68,7 @@ def make_data_list_google_font(image_size, num_style, encoder_path, encoder_zdim
             p = os.path.join('../font2img/image', data_type_df.loc[i, 'font'])
             if os.path.isdir(p) == False: continue
 
-            img = read_img(os.path.join(p, 'A.png'), image_size)
+            img = read_img(os.path.join(p, 'A.png'), image_size, margin=margin)
             with torch.no_grad():
                 feature = model(((img+1)*0.5).unsqueeze(0)).squeeze(0)
             data_list[data_type] += [(img, feature)]
@@ -74,7 +80,7 @@ def make_data_list_google_font(image_size, num_style, encoder_path, encoder_zdim
 
     return data_list
 
-def make_data_list_myfonts(image_size, num_style, encoder_path, encoder_zdim):
+def make_data_list_myfonts(image_size, num_style, encoder_path, encoder_zdim, margin=5):
     data_list = {'train': [], 'test': []}
 
     model = StyleEncoder(encoder_zdim)
@@ -89,7 +95,7 @@ def make_data_list_myfonts(image_size, num_style, encoder_path, encoder_zdim):
             if num_style <= style_i: break
 
             img = np.load(path_list[i])['arr_0'][0] # A のみ
-            img = transform(preprocessing_myfonts(img, img_size=image_size, margin=5)).float()
+            img = transform(preprocessing_myfonts(img, img_size=image_size, margin=margin)).float()
             with torch.no_grad():
                 feature = model(((img+1)*0.5).unsqueeze(0)).squeeze(0)
             data_list[data_type] += [(img, feature)]
@@ -101,13 +107,13 @@ def make_data_list_myfonts(image_size, num_style, encoder_path, encoder_zdim):
 
     return data_list
 
-def make_data_loader(batch_size, image_size, num_style, encoder_path, encoder_zdim, dataset_name='google_fonts', da_rate=0.):
+def make_data_loader(batch_size, image_size, num_style, encoder_path, encoder_zdim, dataset_name='google_fonts', da_rate=0., margin=5):
     print(f'DATASET NAME IS {dataset_name}')
     assert dataset_name in ['google_fonts', 'myfonts'], f'dataset_name must be google_fonts or myfonts. but {dataset_name} is given.'
     if dataset_name == 'google_fonts':
-        data_list = make_data_list_google_font(image_size, num_style, encoder_path, encoder_zdim)
+        data_list = make_data_list_google_font(image_size, num_style, encoder_path, encoder_zdim, margin=margin)
     elif dataset_name == 'myfonts':
-        data_list = make_data_list_myfonts(image_size, num_style, encoder_path, encoder_zdim)
+        data_list = make_data_list_myfonts(image_size, num_style, encoder_path, encoder_zdim, margin=margin)
 
     train_dataset = LoadDataset(data_list['train'], da_rate=da_rate)
     test_dataset = LoadDataset(data_list['test'], da_rate=da_rate)
