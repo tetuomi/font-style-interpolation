c844b5be53cbfabdd17fa927632931a09015907e
tetuomi <kondo.tetta851@mail.kyutech.jp>
2023-12-29 13:50:05 +0900

    Train FANnet

--- style_encoder.py
+++ style_encoder.py
@@ -1,51 +1,38 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 
 class StyleEncoder(nn.Module):
-    def __init__(self, zdim):
+    def __init__(self, num_char):
         super().__init__()
-        self.zdim = zdim
-        hidden_units = 512
 
         self.encoder = nn.Sequential(
-                    nn.Conv2d(1, 32, 4, 2, 1, bias=False),
-                    nn.ReLU(inplace=True),
-                    nn.Conv2d(32, 64, 4, 2, 1, bias=False),
-                    nn.BatchNorm2d(64),
-                    nn.ReLU(inplace=True),
-                    nn.Conv2d(64, 128, 4, 2, 1, bias=False),
-                    nn.BatchNorm2d(128),
-                    nn.ReLU(inplace=True),
-                    nn.Conv2d(128, 256, 4, 1, 1, bias=False),
-                    nn.BatchNorm2d(256),
-                    nn.ReLU(inplace=True),
-        )
-        self.fc_c = nn.Sequential(
-                    nn.Linear(256*7*7, hidden_units),
-                    # nn.ReLU(inplace=True),
-                    nn.Dropout(p=0.5),
-                    nn.Linear(hidden_units, int(zdim)),
-                    # nn.ReLU(inplace=True)
-                    )
-        self.fc_f = nn.Sequential(
-                    nn.Linear(256*7*7, hidden_units),
-                    # nn.ReLU(inplace=True),
-                    nn.Dropout(p=0.5),
-                    nn.Linear(hidden_units, int(zdim)),
-                    # nn.ReLU(inplace=True)
-                    )
-        self.fc_label = nn.Sequential(
-            nn.Linear(26, int(zdim)),
+            nn.Conv2d(1, 32, 4, 2, 1, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(32, 64, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 128, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 256, 4, 1, 1, bias=False),
+            nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
         )
-        self.classifier_c = nn.Sequential(
-                            nn.Linear(int(zdim), 26),
-#                             nn.Softmax(dim=1)
+        self.fc_f = nn.Sequential(
+            nn.Linear(256*7*7, 512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5),
+            nn.Linear(512, 512),
+        )
+        self.fc_label = nn.Sequential(
+            nn.Linear(num_char, 512),
         )
         self.upsample = nn.Sequential(
-            nn.Linear(zdim*2, 256*7*7),
-            nn.ReLU(inplace=True))
+            nn.Linear(512*2, 256*7*7),
+            nn.ReLU(inplace=True)
+        )
         self.decoder = nn.Sequential(
             nn.ConvTranspose2d(256, 128, 4, 1, 1, bias=False),
             nn.BatchNorm2d(128),
@@ -60,11 +47,21 @@ class StyleEncoder(nn.Module):
             nn.Sigmoid()
         )
 
-    def forward(self, x):
-        z = self.encoder(x)
-        z = z.view(z.shape[0],-1)
+    def forward(self, x, label):
+        feat = self.encoder(x)
+        feat = feat.view(feat.shape[0], -1)
+        feat_x = self.fc_f(feat)
+        feat_label = self.fc_label(label)
 
-        z_f = self.fc_f(z)
-        z_f = F.normalize(z_f)
+        feat = torch.cat((feat_x, feat_label), 1)
+        feat = self.upsample(feat).reshape((feat.shape[0], 256, 7, 7))
+        y = self.decoder(feat)
+        
+        return y
 
-        return z_f
+    def style_encode(self, x):
+        feat = self.encoder(x)
+        feat = feat.view(feat.shape[0], -1)
+        feat_x = self.fc_f(feat)
+
+        return feat_x
---
+++ train_fannet.py
@@ -0,0 +1,190 @@
+import os
+import json
+import random
+import requests
+from glob import glob
+
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.utils.data as data
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+from utils import *
+from fannet import FANnet
+from style_encoder import StyleEncoder
+
+
+class LoadDataset(data.Dataset):
+    def __init__(self, data_list, num_char=26, image_size=64, margin=5):
+        self.data_list = data_list
+        self.num_char = num_char
+        self.image_size = image_size
+        self.margin = margin
+
+    def __len__(self):
+        return len(self.data_list)
+
+    def __getitem__(self, index):
+        path = self.data_list[index]['path']
+        char = self.data_list[index]['char']
+        img = np.load(path)['arr_0'][char]
+        img = torch.tensor(preprocessing_myfonts(img, img_size=self.image_size, margin=self.margin))
+        img = img.unsqueeze(0).float()
+
+        pair_path = self.data_list[random.randint(0, len(self.data_list)-1)]['path']
+        pair_img = np.load(pair_path)['arr_0'][random.randint(0, self.num_char-1)]
+        pair_img = torch.tensor(preprocessing_myfonts(pair_img, img_size=self.image_size, margin=self.margin))
+        pair_img = pair_img.unsqueeze(0).float()
+
+        gt_img = np.load(pair_path)['arr_0'][char]
+        gt_img = torch.tensor(preprocessing_myfonts(gt_img, img_size=self.image_size, margin=self.margin))
+        gt_img = gt_img.unsqueeze(0).float()
+        
+        onehot_label = nn.functional.one_hot(torch.tensor(char), num_classes=self.num_char).float()
+
+        return img, pair_img, gt_img, onehot_label
+
+def make_data_list(num_char):
+    data_list = {'train': [], 'val': [], 'test': []}
+
+    for data_type in ['train', 'val', 'test']:
+        path_list = glob(f'../font2img/myfonts/{data_type}/*')
+
+        for i in range(len(path_list)):
+            data_list[data_type] += [{'path': path_list[i], 'char': c} for c in range(num_char)]
+
+    print('TRAIN SIZE: {}'.format(len(data_list['train'])))
+    print('VALID SIZE: {}'.format(len(data_list['val'])))
+    print('TEST SIZE: {}'.format(len(data_list['test'])))
+
+    return data_list
+
+def make_data_loader(batch_size, num_char, image_size, margin=5):
+    data_list = make_data_list(num_char)
+
+    train_dataset = LoadDataset(data_list['train'], num_char=num_char, image_size=image_size, margin=margin)
+    valid_dataset = LoadDataset(data_list['val'], num_char=num_char, image_size=image_size, margin=margin)
+    test_dataset = LoadDataset(data_list['test'], num_char=num_char, image_size=image_size, margin=margin)
+
+    dataloader = {
+        'train': DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=16, pin_memory=True),
+        'val': DataLoader(valid_dataset, batch_size=batch_size, shuffle=False, num_workers=16, pin_memory=True),
+        'test': DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=16, pin_memory=True)
+    }
+
+    return dataloader
+
+def train(model, dataloader_dict, optimizer, num_epochs, log_dir, device, model_path):
+    print('使用デバイス：', device)
+
+    model = model.to(device)
+
+    min_val_loss = 1e9
+    writer = SummaryWriter(log_dir=log_dir)
+    non_updated_cnt = 0
+
+    PATIENT_TIME = 50
+    W_REC = 1e0
+    W_REC2 = 1e0
+
+    for epoch in range(num_epochs):
+        print('')
+
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()
+            else:
+                model.eval()
+
+            epoch_loss = 0.0
+            epoch_rec_loss = 0.0
+            epoch_rec_loss2 = 0.0
+
+            for img, pair, gt, label in dataloader_dict[phase]:
+                img = img.to(device)
+                pair = pair.to(device)
+                gt = gt.to(device)
+                label = label.to(device)
+
+                optimizer.zero_grad()
+
+                with torch.set_grad_enabled(phase == 'train'):
+                    y = model(img, label)
+                    pair_y = model(pair, label)
+
+                    # Compute loss
+                    rec_loss = nn.L1Loss()(y, img)
+                    rec_loss2 = nn.L1Loss()(pair_y, gt)
+
+                    ## summarize loss
+                    loss = W_REC*rec_loss + W_REC2*rec_loss2
+
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                    epoch_rec_loss += rec_loss.item() * img.size(0)
+                    epoch_rec_loss2 += rec_loss2.item() * img.size(0)
+                    epoch_loss += loss.item() * img.size(0)
+
+            epoch_rec_loss = epoch_rec_loss / len(dataloader_dict[phase].dataset)
+            epoch_rec_loss2 = epoch_rec_loss2 / len(dataloader_dict[phase].dataset)
+            epoch_loss = epoch_loss / len(dataloader_dict[phase].dataset)
+
+            writer.add_scalar('loss/' + phase + '/rec', epoch_rec_loss, epoch)
+            writer.add_scalar('loss/' + phase + '/rec2', epoch_rec_loss2, epoch)
+            writer.add_scalar('loss/' + phase + '/total', epoch_loss, epoch)
+
+            print('Epoch {}/{}  {} Loss: {:.4f} (min val loss: {:.4f})'.format(epoch+1, num_epochs, phase, epoch_loss, min_val_loss))
+
+            if phase == 'val' and epoch_loss < min_val_loss:
+                min_val_loss = epoch_loss
+                torch.save(model.state_dict(), model_path)
+                print('model is saved')
+
+            if phase == 'val':
+                if epoch_loss > min_val_loss:
+                    non_updated_cnt += 1
+                    if non_updated_cnt >= PATIENT_TIME:
+                        writer.close()
+                        return
+                else:
+                    non_updated_cnt = 0
+    writer.close()
+
+
+if __name__=='__main__':
+    freeze_seed(1234)
+
+    LR = 1e-4 # 1e-5 when model is fannet
+    EPOCHS = 2000
+    NUM_CHAR = 26
+    IMAGE_SIZE = 64
+    BATCH_SIZE = 512
+    MODEL_NAME = 'fannet2' # 'fannet', 'fannet2'
+    MODEL_PATH = f'./weight/style_encoder_{MODEL_NAME}.pth'
+    DEVICE = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+    
+
+    num_log_dir = str(len(glob('logs/*')) + 1)
+    log_dir = os.path.join('logs', 'log' + num_log_dir)
+    os.makedirs(log_dir, exist_ok=True)
+
+    dataloader_dict = make_data_loader(BATCH_SIZE, NUM_CHAR, image_size=IMAGE_SIZE)
+
+    if MODEL_NAME == 'fannet':
+        model = FANnet(NUM_CHAR)
+    elif MODEL_NAME == 'fannet2':        
+        model = StyleEncoder(NUM_CHAR)
+    else:
+        raise ValueError('MODEL_NAME must be fannet or fannet2')
+    
+    optimizer = torch.optim.Adam(model.parameters(), lr=LR)
+
+    train(model, dataloader_dict, optimizer, EPOCHS, log_dir, DEVICE, MODEL_PATH)
+    
+    # send slack message
+    requests.post(os.getenv('SLACK_URL'), data=json.dumps({'text': f":white_check_mark: log{num_log_dir} All finished !!!"}))
