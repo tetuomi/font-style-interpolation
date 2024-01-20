5889905508b16da87961460ea3a1c28a733e3a5a
tetuomi <kondo.tetta851@mail.kyutech.jp>
2024-01-20 16:36:22 +0900

    Add argment of model path

--- run_eval.sh
+++ run_eval.sh
@@ -4,14 +4,16 @@
 # pipenv run python eval_fannet_weight_interpolation.py
 # pipenv run python eval_fannet_letter_recognition.py
 
+DM_MODEL_PATH='./weight/log39_fannet_retrain_step_700000.pth'
+
 echo 'Noise evaluation'
-pipenv run python eval_dm_weight_interpolation.py -approach 'Noise'
-pipenv run python eval_dm_letter_recognition.py -approach 'Noise'
+pipenv run python eval_dm_weight_interpolation.py -approach 'Noise' -model_path $DM_MODEL_PATH
+pipenv run python eval_dm_letter_recognition.py -approach 'Noise' -model_path $DM_MODEL_PATH
 
 echo 'Condition evaluation'
-pipenv run python eval_dm_weight_interpolation.py -approach 'Condition'
-pipenv run python eval_dm_letter_recognition.py -approach 'Condition'
+pipenv run python eval_dm_weight_interpolation.py -approach 'Condition' -model_path $DM_MODEL_PATH
+pipenv run python eval_dm_letter_recognition.py -approach 'Condition' -model_path $DM_MODEL_PATH
 
 echo 'Image evaluation'
-pipenv run python eval_dm_weight_interpolation.py -approach 'Image'
-pipenv run python eval_dm_letter_recognition.py -approach 'Image'
+pipenv run python eval_dm_weight_interpolation.py -approach 'Image' -model_path $DM_MODEL_PATH
+pipenv run python eval_dm_letter_recognition.py -approach 'Image' -model_path $DM_MODEL_PATH
